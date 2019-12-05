14f943c9fc601bad998224099e7ccff647631322
DanLeonardo <daniel.leonardo.cs@gmail.com>
2019-12-05 12:47:08 -0700

    Completed Day 03

--- 03/part2/wires.py
+++ 03/part2/wires.py
@@ -1,3 +1,5 @@
+import math
+
 def calculate_position(start_pos, offset):
     direction = offset[0]
     distance = int(offset[1:])
@@ -29,9 +31,11 @@ def find_intersections(paths):
 
         # Start at origin
         start_pos_2 = (0, 0)
+        distance_path_2 = 0
         for path_2 in paths[1]:
             # Calculate ending position for step
             end_pos_2 = calculate_position(start_pos_2, path_2)
+            direction_2 = path_2[0]
 
             # Check if lines intersect
             if (direction_1 is 'L' or direction_1 is 'R') and (direction_2 is 'U' or direction_2 is 'D'):
@@ -73,12 +77,22 @@ def find_intersections(paths):
                 if left is not None:
                     if (left[0] <= top[0] <= right[0]) and (top[1] <= left[1] <= bottom[1]):
                         intersect_pos = top[0], left[1]
-                        intersections.append(intersect_pos)
+
+                        partial_distance_1 = math.sqrt(pow(start_pos_1[0] - intersect_pos[0], 2) + pow(start_pos_1[1] - intersect_pos[1], 2))
+                        partial_distance_2 = math.sqrt(pow(start_pos_2[0] - intersect_pos[0], 2) + pow(start_pos_2[1] - intersect_pos[1], 2))
+
+                        total_distance_1 = distance_path_1 + partial_distance_1
+                        total_distance_2 = distance_path_2 + partial_distance_2
+                        total_distance = total_distance_1 + total_distance_2
+
+                        intersections.append((intersect_pos, total_distance))
             # Move position to endpoint
             start_pos_2 = end_pos_2
+            distance_path_2 += int(path_2[1:])
 
         # Move position to endpoint
         start_pos_1 = end_pos_1
+        distance_path_1 += int(path_1[1:])
 
     return intersections
 
@@ -99,11 +113,11 @@ if __name__ == '__main__':
     smallest_distance = -1
     closest_intersection = None
     for intersect in intersections:
-        distance = abs(intersect[0]) + abs(intersect[1])
+        distance = intersect[1]
 
         if smallest_distance < 0 or distance < smallest_distance:
-            closest_intersection = intersect
             smallest_distance = distance
+            closest_intersection = intersect
 
     print(smallest_distance)
     print(closest_intersection)
